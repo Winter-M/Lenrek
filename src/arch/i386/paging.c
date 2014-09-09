4284791c0b99328045d8658081778960c28e9784
Winter-M <registrar_scratch@ponyreactor.tk>
2014-09-09 11:31:55 -0400

    Added paging code for i386 target.

---
+++ src/arch/i386/paging.c
@@ -0,0 +1,63 @@
+
+# include <paging.h>
+
+# define CHK_BIT(V, N)	((V) & PAGE_BIT(N))
+# define SET_BIT(V, N)	((V) |= PAGE_BIT(N))
+# define CLR_VIT(V, N)	((V) &= ~PAGE_BIT(N))
+
+# define PDIDX(ADDR)	((ADDR) >> 22)
+# define PTIDX(ADDR)	(((ADDR) >> 12) & 0x3FF)
+
+# define FLAG_MASK	(0xFFF)
+
+paddr_t paging_get_physical(vaddr_t addr, flags_t *flags) {
+	int pdidx = PDIDX(addr);
+	int ptidx = PTIDX(addr);
+
+	paddr_t *pd = paging_get_pdir();
+	if(!CHK_BIT(pd[pdidx], PD_PRESENT)) {
+		return (paddr_t) -1;
+	}
+
+	paddr_t value;
+	if(CHK_BIT(pd[pdidx], PD_LARGEPAGE)) {
+		value = pd[pdidx];
+	} else {
+		paddr_t *pt = paging_get_ptable(pdidx);
+		if(!CHK_BIT(pt[ptidx], PT_PRESENT)) {
+			return (paddr_t)-1;
+		}
+	}
+	
+	if(flags) *flags = value & FLAG_MASK;
+	return (value & ~FLAG_MASK) | (addr & FLAG_MASK);
+}
+
+paddr_t paging_map_page(vaddr_t addr0, paddr_t addr1, flags_t flags) {
+	if((addr0 & PAGE_MASK) || (addr1 & PAGE_MASK)) {
+		return (paddr_t)-1;
+	}
+
+	int pdidx = PDIDX(addr0);
+	int ptidx = PTIDX(addr0);
+
+	paddr_t *pd = paging_get_pdir();
+
+	paddr_t value;
+	if(CHK_BIT(flags, PD_LARGEPAGE)) {
+		value = pd[pdidx];
+		pd[pdidx] = addr1 | flags;
+	} else {	
+		if(!CHK_BIT(pd[pdidx], PD_PRESENT)) {
+			// TODO : Map page table
+			return (paddr_t)-1;
+		}
+
+		paddr_t *pt = paging_get_ptable(ptidx);
+		value = pt[ptidx];
+		pt[ptidx] = addr1 | flags;
+	}
+
+	return value;
+}
+
